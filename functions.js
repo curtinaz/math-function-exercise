4369a886be492bf81fad8ba37ad22c5e9d22430f
Cristian Curtinaz <63929112+curtinaz@users.noreply.github.com>
2020-12-25 12:05:26 -0300

    various

--- functions.js
+++ functions.js
@@ -7,6 +7,26 @@ let funcoes = {
     Y1: 0,
     X2: 0,
     Y2: 0,
+
+    isitvalid(){
+        X1 = document.querySelector('#x1').value
+        Y1 = document.querySelector('#y1').value
+    
+        X2 = document.querySelector('#x2').value
+        Y2 = document.querySelector('#y2').value
+
+        if (x1.length == 0) {
+            window.alert ("Você deve preencher o valor de x1")
+        } else if (X2.length == 0) {
+            window.alert ("Você deve preencher o valor de x2")
+        }
+        else if (Y1.length == 0) {
+            window.alert ("Você deve preencher o valor de y1")
+        }
+        else if (Y2.length == 0) {
+            window.alert ("Você deve preencher o valor de y2")
+        }
+    },
     
     defineFunc(){
     
@@ -22,6 +42,7 @@ let funcoes = {
         b = (Y1 * X2) - (Y2 * X1) / (X2 - X1);
     
         document.querySelector('#func').innerHTML = `<p>F(x) = ${a}.x + ${b}</p>`;
+        document.querySelector('#funclabel').style = "display:block;"
     
     }
 
